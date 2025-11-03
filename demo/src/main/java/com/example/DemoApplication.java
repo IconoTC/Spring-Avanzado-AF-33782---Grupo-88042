ba6e6fb887b4f6e11a0617a3b6c7986241b906a6
Javier <35864946+jmagit@users.noreply.github.com>
2025-11-03 10:25:39 +0100

    Proyecto creado

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		// ...
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada ...");
+	}
+
+	@Bean
+	CommandLineRunner ejemplos() {
+		return arg -> {
+			System.out.println("Hola mundo");
+		};
+	}
+}
