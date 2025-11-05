790906d7a3f5e14050dc3d70bb2198965dae537c
Javier <35864946+jmagit@users.noreply.github.com>
2025-11-05 10:58:32 +0100

    proyecto demo-data

---
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoDataApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada ...");
+	}
+	
+	@Bean
+	CommandLineRunner ejemplos(EjemplosDatos demos) {
+		return _ -> {
+			demos.run();
+		};
+	}
+
+}
